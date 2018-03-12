a8f978b25c89bbe78b92f9fac3140349406a7e4b
steven.reisman <steven.reisman@philips.com>
2018-03-12 11:36:02 -0400

    Emitting newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'jen@example.com',
-        text: 'Hey this is Steven.'
-    })
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
    console.log('New user connected');
 
-   socket.emit('newMessage', {
-      from: 'mike@example.com',
-       text: 'Hey. What\'s going on?',
-       createdAt: 123
-   });
-
    socket.on('createMessage', (newMessage) => {
       console.log('createMessage', newMessage);
+      io.emit('newMessage', {
+         from: newMessage.from,
+          text: newMessage.text,
+          createdAt: new Date().getTime()
+      });
    });
 
    socket.on('disconnect', () => {
