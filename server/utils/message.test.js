c926d7cc4e6dc1b8d16ec4ebfe0c82a258f9ca3e
steven.reisman <steven.reisman@philips.com>
2018-03-13 15:17:34 -0400

    Adding geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -12,5 +12,19 @@ describe('generateMessage', () => {
         expect(message.from).toBe('Steven');
         expect(message.text).toBe('Hi there.');
         expect(message.createdAt).toBeA('number');
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var latitude = 1;
+        var longitude = 1;
+        var from = 'Admin';
+
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.from).toBe(from);
+        expect(message.createdAt).toBeA('number');
+        expect(message.url).toBe('http://www.google.com/maps?q=1,1')
     });
 });
